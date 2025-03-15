e3dfc8181701cda18dd3d0af4b13febd0519c0cb
Christopher Owens <cowensdev@gmail.com>
2025-03-15 10:02:05 -0400

    Cleanup

---
+++ groovy/classes/app.groovy
@@ -0,0 +1,12 @@
+// create an instance of a Developer
+Developer d = new Developer()
+
+d.first = "Chris"
+d.last = "Owens"
+
+// assign some languages
+d.languages << "Groovy"
+d.languages << "Java"
+
+// call some methods
+d.work()
